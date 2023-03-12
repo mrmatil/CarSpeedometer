b8a3b3fdfe87046fe28ab42116a4004945bcaf12
Mateusz Łukasiński <mateusz.lukasinski@icloud.com>
2023-03-12 23:38:37 +0100

    Initial Commit

---
+++ CarSpeedometer/CarSpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarSpeedometerApp.swift
+//  CarSpeedometer
+//
+//  Created by Mateusz Łukasiński on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarSpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
